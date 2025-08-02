a9b572d11b7dbd9d92be21efa5eadc85adb24582
Jack Westbrook <jack.westbrook@gmail.com>
2025-08-02 15:54:44 +0200

    feat: setup dev tooling

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    ignore: ['dist', 'coverage', 'node_modules'],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      'no-case-declarations': 'off',
+    },
+  },
+]);
